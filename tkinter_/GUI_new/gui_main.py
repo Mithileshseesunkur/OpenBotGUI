1b39ea80b932eead81a9f26d736451cb6c26736b
Mithileshseesunkur <163191079+Mithileshseesunkur@users.noreply.github.com>
2024-08-28 07:13:01 +0000

    new

--- tkinter_/GUI_new/gui_main.py
+++ tkinter_/GUI_new/gui_main.py
@@ -1,50 +1,31 @@
-#from input_frame import my_input_frame
+from input_frame import my_input_frame
 import customtkinter as CTK
 
-class my_input_frame(CTK.CTkFrame):
+# class my_input_frame(CTK.CTkFrame):
 
-    input_length:int
-    entries:list=[]
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
+#     input_length:int
+#     entries:list=[]
+#     def __init__(self, master, **kwargs):
+#         super().__init__(master, **kwargs)
 
-        # add widgets onto the frame
-        self.inputs = ["input_1", "input_2", "input_3", "input_4"]
-        self.input_length=len(self.inputs)
-
-        
-        
-
-        for label_no in range(len(self.inputs)):
-            self.label = CTK.CTkLabel(self, text=self.inputs[label_no])
-            self.label.grid(row=label_no, column=0, pady=20, padx=20)
-
-            self.entry = CTK.CTkEntry(self)
-            self.entry.grid(row=label_no, column=1, pady=20, padx=20)
-    
-            self.entries.append(self.entry)
-    
-
-class my_send_button_frame(CTK.CTkFrame):
-    
-    
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
+#         # add widgets onto the frame
+#         self.inputs = ["input_1", "input_2", "input_3", "input_4"]
+#         self.input_length=len(self.inputs)
 
         
-        #self.inputs=
+        
 
-        #self.input_frame = my_input_frame
+#         for label_no in range(len(self.inputs)):
+#             self.label = CTK.CTkLabel(self, text=self.inputs[label_no])
+#             self.label.grid(row=label_no, column=0, pady=20, padx=20)
 
-        def print_inputs():
+#             self.entry = CTK.CTkEntry(self)
+#             self.entry.grid(row=label_no, column=1, pady=20, padx=20)
+    
+#             self.entries.append(self.entry)
+    
 
-            for entry_no in range(0,my_input_frame.input_length):
-                value=my_input_frame.entries.get()
-                print("input_" + str(entry_no)+f": {value}")
-                
 
-        self.send_button = CTK.CTkButton(self, text="Send inputs", command=print_inputs)
-        self.send_button.grid(row=0, column=0, pady=20, padx=20)
 
 
 
@@ -60,8 +41,7 @@ class App(CTK.CTk):
         self.gui_inputs_frame = my_input_frame(master=self)
         self.gui_inputs_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
-        self.gui_send_button_frame = my_send_button_frame(master=self)
-        self.gui_send_button_frame.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
+       
 
 
 
--- tkinter_/GUI_new/input_frame.py
+++ tkinter_/GUI_new/input_frame.py
@@ -1,12 +1,30 @@
 import customtkinter as CTK
 
 class my_input_frame(CTK.CTkFrame):
+    input_length: int
+    entries: list = []
+
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
         # add widgets onto the frame
-        inputs = ["input_1", "input_2", "input_3", "input_4"]
+        self.inputs = ["input_1", "input_2", "input_3", "input_4"]
+        self.input_length = len(self.inputs)
 
-        for i in range(len(inputs)):
-            self.label = CTK.CTkLabel(self, text=inputs[i])
-            self.label.grid(row=i, column=0, pady=20, padx=20)
+        for label_no in range(len(self.inputs)):
+            self.label = CTK.CTkLabel(self, text=self.inputs[label_no])
+            self.label.grid(row=label_no, column=0, pady=20, padx=20)
+
+            self.entry = CTK.CTkEntry(self)
+            self.entry.grid(row=label_no, column=1, pady=20, padx=20)
+
+            self.entries.append(self.entry)
+
+        def print_values():
+            
+            for entry_no in range(input_frame.input_length):
+                value = input_frame.entries[entry_no].get()
+                print(f"input_{entry_no + 1}: {value}")
+
+        self.send_button = CTK.CTkButton(self, text="Send inputs", command=print_inputs)
+        self.send_button.grid(row=label_no+1, column=0, pady=20, padx=20)
